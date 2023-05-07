a946d196af40d6a21a00514441c62eaec89577b7
alwalk0 <walkernotwalkeer@gmail.com>
2023-05-07 16:38:02 +0200

    add auth middleware

--- columbus/main.py
+++ columbus/main.py
@@ -7,6 +7,11 @@ from starlette.responses import PlainTextResponse
 import databases
 import contextlib
 from columbus.start import MAIN_CONFIG_NAME
+import base64
+from starlette.authentication import SimpleUser, AuthCredentials, AuthenticationError, AuthenticationBackend
+from starlette.middleware import Middleware
+from starlette.middleware.authentication import AuthenticationMiddleware
+import binascii
 
 
 @contextlib.asynccontextmanager
@@ -14,6 +19,26 @@ async def lifespan(app):
     await database.connect()
     yield
     await database.disconnect()
+
+class BasicAuthBackend(AuthenticationBackend):
+    async def authenticate(self, conn):
+        if "Authorization" not in conn.headers:
+            return
+
+        auth = conn.headers["Authorization"]
+        try:
+            scheme, credentials = auth.split()
+            if scheme.lower() != 'basic':
+                return
+            decoded = base64.b64decode(credentials).decode("ascii")
+        except (ValueError, UnicodeDecodeError, binascii.Error) as exc:
+            raise AuthenticationError('Invalid basic auth credentials')
+
+        username, _, password = decoded.partition(":")
+        # TODO: You'd want to verify the username and password here.
+        return AuthCredentials(["authenticated"]), SimpleUser(username)
+
+
 
 
 config_dict = read_config(MAIN_CONFIG_NAME)
@@ -36,5 +61,9 @@ database = databases.Database(validated_config["database"])
 validated_config["database"] = database #replace the database url in config with the actual Database object
 
 routes = create_routes_list(validated_config)
+
+middleware = [
+    Middleware(AuthenticationMiddleware, backend=BasicAuthBackend())
+]
 
 app = Starlette(routes=routes, lifespan=lifespan)
